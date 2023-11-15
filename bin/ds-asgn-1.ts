79fd452cafebde93a3842fb38681907d27435864
georginawalsh <20093147>
2023-11-15 10:43:47 +0000

    URL for lambda created.

--- bin/ds-asgn-1.ts
+++ bin/ds-asgn-1.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { DsAsgn1Stack } from '../lib/ds-asgn-1-stack';
+import { Asgn01AppStack } from '../lib/ds-asgn-1-stack';
 
 const app = new cdk.App();
-new DsAsgn1Stack(app, 'DsAsgn1Stack', {
+new Asgn01AppStack(app, 'Asgn01AppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- lib/ds-asgn-1-stack.ts
+++ lib/ds-asgn-1-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambdanode from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class DsAsgn1Stack extends cdk.Stack {
+export class Asgn01AppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const simpleFn = new lambdanode.NodejsFunction(this, "SimpleFn", {
+      architecture: lambda.Architecture.ARM_64,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambda/simple.ts`,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DsAsgn1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const simpleFnURL = simpleFn.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "Simple Function Url", { value: simpleFnURL.url });
+
   }
 }
