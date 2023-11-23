1cb8e9838e4a3a3dd933dfdcac67fa962aba138d
georginawalsh <20093147>
2023-11-23 02:13:24 +0000

    Added getMovieReview, and updated type and util in shared.

--- lambda/getAllMovies.ts
+++ lambda/getAllMovies.ts
@@ -1,33 +1,30 @@
-import { Handler } from "aws-lambda";
-import { APIGatewayProxyHandlerV2 } from "aws-lambda"; 
-
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";  // CHANGED
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
 
-const ddbDocClient = createDDbDocClient();
+const ddbClient = new DynamoDBClient({ region: process.env.REGION });
 
-export const handler: Handler = async (event, context) => {
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => { // CHANGED
   try {
+    // Print Event
     console.log("Event: ", event);
-    const parameters = event?.queryStringParameters;
-    
-    const commandOutput = await ddbDocClient.send(
 
-        new ScanCommand({
-            TableName: process.env.TABLE_NAME,
-          })
+    const commandOutput = await ddbClient.send(
+      new ScanCommand({
+        TableName: process.env.TABLE_NAME,
+      })
     );
-    if (!commandOutput) {
+    if (!commandOutput.Items) {
       return {
         statusCode: 404,
         headers: {
           "content-type": "application/json",
         },
-        body: JSON.stringify({ Message: "Invalid input" }),
+        body: JSON.stringify({ Message: "Invalid movie Id" }),
       };
     }
     const body = {
-      data: commandOutput,
+      data: commandOutput.Items,
     };
 
     // Return Response
--- lambda/getMovieById.ts
+++ lambda/getMovieById.ts
@@ -1,15 +1,29 @@
-import { Handler } from "aws-lambda";
+// import { Handler } from "aws-lambda";
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
 
-import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import {
+  QueryCommand,
+  QueryCommandInput,
+} from "@aws-sdk/lib-dynamodb";
+
+import Ajv from "ajv";
+import schema from "../shared/types.schema.json";
+
+const ajv = new Ajv();
+const isValidQueryParams = ajv.compile(
+  schema.definitions["MovieAndCastMemberQueryParams"] || {}
+);
 
 const ddbDocClient = createDDbDocClient();
 
-export const handler: APIGatewayProxyHandlerV2 = async (event, context) =>{
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => { // Note change
   try {
     console.log("Event: ", event);
+    // const parameters = event?.queryStringParameters;
+    // const movieId = parameters ? parseInt(parameters.movieId) : undefined;
     const parameters  = event?.pathParameters;
     const movieId = parameters?.movieId ? parseInt(parameters.movieId) : undefined;
 
@@ -22,12 +36,66 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) =>{
         body: JSON.stringify({ Message: "Missing movie Id" }),
       };
     }
+
+
+    const queryParams = event.queryStringParameters;
+    if (!queryParams) {
+      return {
+        
+        // statusCode: 500,
+        // headers: {
+        //   "content-type": "application/json",
+        // },
+        // body: JSON.stringify({ message: "Missing query parameters" }),
+      };
+    }
+    if (!isValidQueryParams(queryParams)) {
+      return {
+      //   statusCode: 500,
+      //   headers: {
+      //     "content-type": "application/json",
+      //   },
+      //   body: JSON.stringify({
+      //     message: `Incorrect type. Must match Query parameters schema`,
+      //     schema: schema.definitions["MovieAndCastMemberQueryParams"],
+      //   }),
+      // };
+      } 
+    }
+
+    const cast = queryParams;
+    let commandInput: QueryCommandInput = {
+      TableName: process.env.CAST_TABLE_NAME,
+    };
+    if ("cast" in queryParams) {
+      commandInput = {
+        ...commandInput,
+        // IndexName: "roleIx",
+        KeyConditionExpression: " movieId = :m ",
+        ExpressionAttributeValues: {
+          ":m": movieId,
+          cast: new QueryCommand(commandInput)
+        },
+      };
+      // const additionalCommandOutput = await ddbDocClient.send(
+      // new QueryCommand(commandInput)
+      // );
+    }
+
+
+
     const commandOutput = await ddbDocClient.send(
       new GetCommand({
         TableName: process.env.TABLE_NAME,
         Key: { movieId: movieId },
-      })
+      }),
     );
+
+    // const additionalCommandOutput = await ddbDocClient.send(
+    //   new QueryCommand(commandInput)
+    //   );
+    
+    console.log("GetCommand response: ", commandOutput);
     if (!commandOutput.Item) {
       return {
         statusCode: 404,
@@ -38,7 +106,10 @@ export const handler: APIGatewayProxyHandlerV2 = async (event, context) =>{
       };
     }
     const body = {
-      data: commandOutput.Item,
+      movie: commandOutput.Item,
+      // cast: new QueryCommand(commandInput)
+      // cast: commandInput
+      // cast: additionalCommandOutput
     };
 
     // Return Response
